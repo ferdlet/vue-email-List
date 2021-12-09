c74baa3d58bc9cfcdb6dbfb5a18c3fd7a956743d
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-12-09 16:00:55 +0100

    bonus 1

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@ const app = new Vue({
     el: '#root',
     data: {
         emails: [],
+        emailsFinal: [],
+        numEmails: 10,
     },
     methods: {
         
     },
     created() {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.numEmails; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
                 // handle success
                 this.emails.push(response.data.response);
+                if (this.emails.length == this.numEmails) {
+                    this.emailsFinal = [...this.emails]
+                }
             })
             .catch(function (error) {
                 // handle error
