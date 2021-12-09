19f3a8f82d171565f01b4e9e6518176702704fd3
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-12-09 15:29:13 +0100

    init + inserimento email dinamico

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        emails: [],
+    },
+    methods: {
+        
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // handle success
+                this.emails.push(response.data.response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+        }
+    }
+});
